a3c6da2983a660e4eaad0347807116f801645807
shenwei356 <shenwei356@gmail.com>
2013-04-16 14:04:23 +0800

    submit

---
+++ README.md
@@ -0,0 +1,41 @@
+dirsize
+========
+
+Summarize size of directories and files in directories.
+
+Install
+-------
+This package is "go-gettable", just:
+
+    go get github.com/shenwei356/util/dirsize
+
+Usage
+-----
+    
+    Usage: dirsize [OPTION]... [DIR]...
+
+    Summarize size of directories and files in directories.
+
+    OPTION:
+      -a=false: sort by Alphabet.
+      -r=false: sort by Size.
+      -s=true: reverse order while sorting.
+
+Example
+-------
+    
+    dirsize -s -r .
+
+Result:
+
+    .:   6.54 KB
+      0.00  B	t.txt
+    505.00  B	.gitattributes
+    596.00  B	README.md
+    628.00  B	.git
+      2.08 KB	.gitignore
+      2.77 KB	main.go
+
+Copyright (c) 2013, Wei Shen (shenwei356@gmail.com)
+
+[MIT License](https://github.com/shenwei356/dirsize/blob/master/LICENSE)
---
+++ main.go
@@ -0,0 +1,119 @@
+// Copyright 2013 Wei Shen (shenwei356@gmail.com). All rights reserved.
+// Use of this source code is governed by a MIT-license
+// that can be found in the LICENSE file.
+
+// Summarize size of directories and files in directories.
+package main
+
+import (
+    . "github.com/shenwei356/util/bytesize"
+    . "github.com/shenwei356/util/sortitem"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+var (
+	sortByAlphabet bool
+	sortBySize     bool
+	sortReverse    bool
+)
+
+// Parse arguments and show usage.
+func init() {
+	flag.BoolVar(&sortByAlphabet, "a", false, "sort by Alphabet.")
+	flag.BoolVar(&sortBySize, "s", true, "sort by Size.")
+	flag.BoolVar(&sortReverse, "r", false, "reverse order while sorting.")
+	flag.Usage = func() {
+		fmt.Printf("\nUsage: %s [OPTION]... [DIR]...\n\n", os.Args[0])
+		fmt.Println("Summarize size of directories and files in directories.")
+		fmt.Println("by Wei Shen (shenwei356@gmail.com)\n")
+		fmt.Println("OPTION:")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+	}
+}
+
+func main() {
+	for _, arg := range flag.Args() {
+		if strings.HasPrefix(arg, "-") {
+			continue
+		}
+		if _, err := os.Stat(arg); err == nil {
+			size, info, err := FolderSize(arg)
+			if err != nil {
+				fmt.Println(err)
+			}
+            // reverse order while sorting
+			if sortReverse {
+				if sortBySize { // sort by Alphabet
+					sort.Sort(ByValue{info})
+				} else {        // sort by Size
+					sort.Sort(Reverse{ByKey{info}})
+				}
+			} else {
+				if sortBySize {
+					sort.Sort(Reverse{ByValue{info}})
+				} else {
+					sort.Sort(ByKey{info})
+				}
+			}
+
+			fmt.Printf("\n%s: %v\n", arg, ByteSize(size))
+			for _, item := range info {
+				fmt.Printf("%v\t%s\n", ByteSize(item.Value), item.Key)
+			}
+
+		} else {
+			fmt.Printf("\n%s: NOT exists!\n", arg)
+		}
+	}
+}
+
+// Get total size of files in a directory, and store the sizes of first level
+// directories and files in a key-value list.
+func FolderSize(dirname string) (float64, []Item, error) {
+	var size float64 = 0
+	var info []Item
+	info = make([]Item, 0)
+
+	bytes, err := ioutil.ReadFile(dirname)
+	if err == nil {
+		size1 := float64(len(bytes))
+		info = append(info, Item{dirname, size1})
+		return size1, info, nil
+	}
+
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		recover()
+		return 0, nil, errors.New("ReadDir Error!")
+	}
+	for _, file := range files {
+		if file.Name() == "." || file.Name() == ".." {
+			continue
+		}
+		if file.IsDir() {
+			size1, _, err := FolderSize(filepath.Join(dirname, file.Name()))
+			if err != nil {
+				recover()
+				return 0, nil, errors.New("ReadDir Error!")
+			}
+			size += size1
+			info = append(info, Item{file.Name(), size1})
+		} else {
+			size1 := float64(file.Size())
+			size += size1
+			info = append(info, Item{file.Name(), size1})
+		}
+	}
+	return size, info, nil
+}
